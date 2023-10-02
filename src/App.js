a93487dfe7da088bcdac99f04354235f9751bed3
Jensenvembil <jensenvs3@gmail.com>
2023-10-02 19:11:38 +0530

    new branch update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  window.location.href = 'https://www.linkedin.com/in/jensen-vembil-5243344a';
+  window.location.href = 'https://www.linkedin.com/in/jensen-vembil-5243344a/';
 }
 
 export default App;
